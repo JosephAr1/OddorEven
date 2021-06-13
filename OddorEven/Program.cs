6cf2148de2dd9f6256ef3b157149731893f1e84a
JosephAr1 <jonielhet@yahoo.com>
2021-06-13 16:44:19 +1200

    Add project files.

---
+++ OddorEven/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OddorEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, remainder;
+            Console.WriteLine("Please enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            remainder = number % 2;
+
+            if (remainder == 0)
+            {
+                Console.WriteLine("{0} is an Even number", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an Odd number", number);
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
